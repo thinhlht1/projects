d42d35680d7ae7959d78d271a29488ca0642f575
Thinh Le <thinhlht1@gmail.com>
2020-01-23 00:16:50 +0700

    testing on heroku

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+key = 'key'
+data = 'randomString'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(BASE_DIR, "storage")
+listPath = os.path.join(path, "list")
+pathToFile = os.path.join(listPath, "{}.txt".format(key))
+os.system("echo {} > {}".format(data, pathToFile))

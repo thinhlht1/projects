99bf50ee073b0a3b0ed1b34254d94be66afe8743
Thinh Le <thinhlht1@gmail.com>
2020-01-23 00:18:45 +0700

    testing on heroku

--- hello/views.py
+++ hello/views.py
@@ -561,7 +561,7 @@ def writeData(pathToFile, data, mode, key, time, appendToRAM = True):
     try:
         availableModes.index(mode)
         if mode == "w":
-            os.system("echo {} > {}".format(data, pathToFile))
+            os.system("echo {} >> {}".format(data, pathToFile))
         # fh = open(pathToFile, mode)
         # data = fh.write(data)
             keyTime[key] = time

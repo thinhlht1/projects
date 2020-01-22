ffc1333fe668796b986aada31b83e8aefa84970f
Thinh Le <thinhlht1@gmail.com>
2020-01-22 14:36:11 +0700

    Change path

--- hello/views.py
+++ hello/views.py
@@ -9,8 +9,8 @@ import glob
 import os
 import re
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path = os.path.join(BASE_DIR, "storage")
-
 metadata = os.path.join(BASE_DIR, "metadata")
 
 keyTime = {}

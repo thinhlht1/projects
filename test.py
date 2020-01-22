7b972ebd88326311120751574a248cc69f3cf298
Thinh Le <thinhlht1@gmail.com>
2020-01-23 00:59:01 +0700

    Try again

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ data = 'randomString'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path = os.path.join(BASE_DIR, "storage")
 listPath = os.path.join(path, "list")
-os.system('cd listPath')
+os.system('cd {}'.format(listPath))
 os.system('echo {} > {}.txt'.format(data, key))
 os.system('cd {}'.format(BASE_DIR))
